96fe5cab74f886604544423029b1c18090bd694a
Danil Nemirovsky <danil.nemirovsky@gmail.com>
2023-05-13 05:22:23 +0100

    Add block at height parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use reqwest::Error;
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    jsonrpc: String,
+    id: i32,
+    result: Result,
+}
+
+#[derive(Deserialize, Debug)]
+struct Result {
+    block_id: BlockId,
+    block: Block,
+}
+
+#[derive(Deserialize, Debug)]
+struct BlockId {
+    hash: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Block {
+    header: Header,
+}
+
+#[derive(Deserialize, Debug)]
+struct Header {
+    chain_id: String,
+    height: String,
+    proposer_address: String,
+}
+
+#[tokio::main]
+async fn main() -> core::result::Result<(), Error> {
+    let request_url = format!("https://rpc.osmosis.zone/block?height={height}",
+                              height = "9558629");
+    println!("{}", request_url);
+    let raw_response = reqwest::get(&request_url).await?;
+
+    let response: Response = raw_response.json().await?;
+    println!("{:?}", response);
+    Ok(())
 }
