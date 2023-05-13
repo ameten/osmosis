4b6b2f80aa72d1dbc5d59048545ca7264f069c91
Danil Nemirovsky <danil.nemirovsky@gmail.com>
2023-05-13 08:12:19 +0100

    Add indexing per block height and store into postgres

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-use reqwest::Error;
+use std::time::Duration;
+
 use serde::Deserialize;
 use serde_aux::prelude::*;
-use tokio_postgres::{Client, NoTls};
+use tokio::{task, time};
 
 #[derive(Deserialize, Debug)]
 struct Response {
-    jsonrpc: String,
-    id: i32,
     result: Result,
 }
 
 #[derive(Deserialize, Debug)]
 struct Result {
-    block_id: BlockId,
     block: Block,
-}
-
-#[derive(Deserialize, Debug)]
-struct BlockId {
-    hash: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -28,11 +21,8 @@ struct Block {
 
 #[derive(Deserialize, Debug)]
 struct Header {
-    chain_id: String,
-
     #[serde(deserialize_with = "deserialize_number_from_string")]
     height: i64,
-
     proposer_address: String,
 }
 
@@ -42,15 +32,77 @@ struct ProposerToHeight {
 }
 
 const OSMOSIS_LOWEST_HEIGHT: i64 = 9558628;
+const INDEXER_INTERVAL: u64 = 5;
+
+#[derive(Debug)]
+enum Error {
+    CouldNotCreateHttpClient,
+    CouldNotBuildHttpRequest,
+    CouldNotGetBlockAtHeight,
+    CouldNotParseResponseForBlockAtHeight,
+
+    CouldNotCreateDatabaseClient,
+    CouldNotFindIndexedHeight,
+    CouldNotIndexDuplicateHeight,
+    InsertedIncorrectNumberOfRows,
+}
 
 #[tokio::main]
-async fn main() {
-    let request_url = format!("https://rpc.osmosis.zone/block?height={height}",
-                              height = OSMOSIS_LOWEST_HEIGHT);
-    println!("{}", request_url);
-    let raw_response = reqwest::get(&request_url).await.unwrap();
+async fn main() -> core::result::Result<(), Error> {
+    let http_client = reqwest::Client::builder()
+        .build()
+        .map_err(|_| Error::CouldNotCreateHttpClient)?;
 
-    let response: Response = raw_response.json().await.unwrap();
+    let (database_client, database_connection) =
+        tokio_postgres::connect("host=localhost port=5432 user=osmosis password=osmosis", tokio_postgres::NoTls)
+            .await.map_err(|_| Error::CouldNotCreateDatabaseClient)?;
+
+    tokio::spawn(async move {
+        if let Err(e) = database_connection.await {
+            println!("connection error: {e}");
+        }
+    });
+
+    let forever = task::spawn(async move {
+        let mut interval = time::interval(Duration::from_secs(INDEXER_INTERVAL));
+
+        loop {
+            interval.tick().await;
+            index(&http_client, &database_client)
+                .await
+                .unwrap_or_else(|e| println!("Indexing error {e:?}"));
+        }
+    });
+
+    forever.await.expect("Recurring task failed");
+    Ok(())
+}
+
+async fn index(http_client: &reqwest::Client, database_client: &tokio_postgres::Client)
+               -> core::result::Result<(), Error> {
+    let height_indexed: i64 = database_client
+        .query("SELECT max(height) FROM proposer_to_height", &[])
+        .await
+        .map_err(|_| Error::CouldNotFindIndexedHeight)?
+        .get(0)
+        .map_or_else(|| OSMOSIS_LOWEST_HEIGHT - 1, |r| r.get(0));
+
+    let height_to_index = height_indexed + 1;
+    println!("height_to_index: {height_to_index}");
+
+    let request_url = format!("https://rpc.osmosis.zone/block?height={height_to_index}");
+    println!("request_url: {}", request_url);
+
+    let request = http_client.get(&request_url).build()
+        .map_err(|_| Error::CouldNotBuildHttpRequest)?;
+
+    let raw_response = http_client.execute(request)
+        .await
+        .map_err(|_| Error::CouldNotGetBlockAtHeight)?;
+
+    let response: Response = raw_response.json()
+        .await
+        .map_err(|_| Error::CouldNotParseResponseForBlockAtHeight)?;
     println!("{:?}", response);
 
     let proposer_to_height = ProposerToHeight {
@@ -58,19 +110,17 @@ async fn main() {
         height: response.result.block.header.height,
     };
 
-    let (client, connection) =
-        tokio_postgres::connect("host=localhost port=5432 user=osmosis password=osmosis", NoTls).await.unwrap();
-
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    let rows = client
+    let count_rows_inserted = database_client
         .execute("INSERT INTO proposer_to_height(proposer, height) VALUES ($1, $2)",
                  &[&proposer_to_height.proposer, &proposer_to_height.height])
-        .await.unwrap();
+        .await
+        .map_err(|_| Error::CouldNotIndexDuplicateHeight)?;
 
-    println!("{:?}", rows);
+    println!("{:?}", count_rows_inserted);
+
+    if count_rows_inserted != 1 {
+        return Err(Error::InsertedIncorrectNumberOfRows);
+    }
+
+    Ok(())
 }
