1f7a2328ff59840793e37cd3e8faf03b138dee40
Danil Nemirovsky <danil.nemirovsky@gmail.com>
2023-05-13 06:57:57 +0100

    Save proposer to height into postgres

---
+++ database/00_init.sql
@@ -0,0 +1,4 @@
+create table proposer_to_height (
+    proposer varchar(255),
+    height bigint UNIQUE
+);
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use serde::Deserialize;
 use reqwest::Error;
+use serde::Deserialize;
+use serde_aux::prelude::*;
+use tokio_postgres::{Client, NoTls};
 
 #[derive(Deserialize, Debug)]
 struct Response {
@@ -27,20 +29,48 @@ struct Block {
 #[derive(Deserialize, Debug)]
 struct Header {
     chain_id: String,
-    height: String,
+
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    height: i64,
+
     proposer_address: String,
 }
 
-const OSMOSIS_LOWEST_HEIGHT: u128 = 9558628;
+struct ProposerToHeight {
+    proposer: String,
+    height: i64,
+}
+
+const OSMOSIS_LOWEST_HEIGHT: i64 = 9558628;
 
 #[tokio::main]
-async fn main() -> core::result::Result<(), Error> {
+async fn main() {
     let request_url = format!("https://rpc.osmosis.zone/block?height={height}",
                               height = OSMOSIS_LOWEST_HEIGHT);
     println!("{}", request_url);
-    let raw_response = reqwest::get(&request_url).await?;
+    let raw_response = reqwest::get(&request_url).await.unwrap();
 
-    let response: Response = raw_response.json().await?;
+    let response: Response = raw_response.json().await.unwrap();
     println!("{:?}", response);
-    Ok(())
+
+    let proposer_to_height = ProposerToHeight {
+        proposer: response.result.block.header.proposer_address,
+        height: response.result.block.header.height,
+    };
+
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost port=5432 user=osmosis password=osmosis", NoTls).await.unwrap();
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let rows = client
+        .execute("INSERT INTO proposer_to_height(proposer, height) VALUES ($1, $2)",
+                 &[&proposer_to_height.proposer, &proposer_to_height.height])
+        .await.unwrap();
+
+    println!("{:?}", rows);
 }
